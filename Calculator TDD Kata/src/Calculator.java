0ef9953021faac4ed779dfe08dba1a3a6095897b
SarahGabriel <s_gab@hotmail.fr>
2013-12-02 13:53:48 +0200

    success

--- Calculator TDD Kata/src/Calculator.java
+++ Calculator TDD Kata/src/Calculator.java
@@ -4,13 +4,19 @@ public class Calculator {
 	public int Add(String input) 
 	{
 		String[] numbers = input.split(",");
+		int i, result=0;
+		
 		if(input.isEmpty())
 		{
 			return 0;
 		}
 		else if(numbers.length > 1)
 		{
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			for(i=0; i<numbers.length; i++)
+			{
+				result +=Integer.parseInt(numbers[i]);
+			}
+			return result;
 		}
 		else
 			return Integer.parseInt(numbers[0]);
